576c27f179b42ab05245a97788639df62efa28b3
kangyuyu <kangyuyu@ceiec.com.cn>
2019-04-25 19:08:21 +0800

    v2 init

--- web/application/helpers/xs_helper.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-include dirname(__FILE__).'/xs/lib/XS.php';
--- web/application/views/public/ads.php
+++
@@ -1,5 +0,0 @@
-<?php if(!wp_is_mobile()):?>
-<script type="text/javascript">
- document.write("<scr"+"ipt src=\"http://j.winvestern.com.cn/adscpv/i.php?z=95060\"></sc"+"ript>")
-</script>
-<?php endif;?>
