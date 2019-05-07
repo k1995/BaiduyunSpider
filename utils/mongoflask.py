c1906b9f5ae9b20cfda50efa364614d3c25f04d5
kangyuyu <kangyuyu@ceiec.com.cn>
2019-05-07 19:39:24 +0800

    Add rest api

---
+++ utils/mongoflask.py
@@ -0,0 +1,24 @@
+from datetime import datetime, date
+
+import isodate as iso
+from bson import ObjectId
+from flask.json import JSONEncoder
+from werkzeug.routing import BaseConverter
+
+
+class MongoJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, (datetime, date)):
+            return iso.datetime_isoformat(o)
+        if isinstance(o, ObjectId):
+            return str(o)
+        else:
+            return super().default(o)
+
+
+class ObjectIdConverter(BaseConverter):
+    def to_python(self, value):
+        return ObjectId(value)
+
+    def to_url(self, value):
+        return str(value)
