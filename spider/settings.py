65212a36f0b37db4802c1c65847c400b1cc07e59
k1995 <k1995328@gmail.com>
2019-05-27 23:04:09 +0800

    Update DOWNLOAD_DELAY, 防止被ban

--- spider/settings.py
+++ spider/settings.py
@@ -32,7 +32,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
