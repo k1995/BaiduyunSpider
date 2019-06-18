63403cb266450902e798225662f3bdb4512647a2
k1995 <k1995328@gmail.com>
2019-06-18 21:38:46 +0800

    Fix normalize url bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,8 +5,11 @@ def normalize_shareurl(url):
     base_url = "https://pan.baidu.com"
     u = parse.urlparse(url)
     if u.path.startswith('/s/'):
-        return "{}/s/{}".format(base_url, u.path)
+        return "{}{}".format(base_url, u.path)
     elif u.path.startswith('/share/link'):
         query = parse.parse_qs(u.query)
-        return "{}/share/link?shareid={}&uk={}".format(base_url, query.shareid, query.uk)
+        return "{}/share/link?shareid={}&uk={}".format(base_url, query['shareid'][0], query['uk'][0])
     raise Exception("URL格式错误")
+
+print(normalize_shareurl("https://pan.baidu.com/share/link?shareid=2101811667&uk=3660825403&zz1"))
+print(normalize_shareurl("https://pan.baidu.com/s/1GxTj91_RT50aIaSG3BmcSA?fid=735147087821718"))
