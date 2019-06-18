6b120d858b754aff6880fab67a55da22dbb2cd53
k1995 <k1995328@gmail.com>
2019-06-18 21:40:29 +0800

    Remove test codes

--- utils/__init__.py
+++ utils/__init__.py
@@ -10,6 +10,3 @@ def normalize_shareurl(url):
         query = parse.parse_qs(u.query)
         return "{}/share/link?shareid={}&uk={}".format(base_url, query['shareid'][0], query['uk'][0])
     raise Exception("URL格式错误")
-
-print(normalize_shareurl("https://pan.baidu.com/share/link?shareid=2101811667&uk=3660825403&zz1"))
-print(normalize_shareurl("https://pan.baidu.com/s/1GxTj91_RT50aIaSG3BmcSA?fid=735147087821718"))
